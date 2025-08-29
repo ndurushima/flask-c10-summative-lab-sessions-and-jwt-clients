42215b179e5dc86c73a496a7fb041b833bce2ca1
ndurushima <ndu1994@gmail.com>
2025-08-29 08:53:44 -0500

    finally got db to work and seed to workgit add .

--- auth-server/config.py
+++ auth-server/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "dev-jwt-secret")
     DEBUG = False
